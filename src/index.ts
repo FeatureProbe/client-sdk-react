0125402251b39513a103c8524f941b1606a977d4
frankzhaopeng <poryzhao@featureprobe.com>
2023-03-23 17:06:33 +0800

    :sparkles:feat: distinguish UA from JS SDK

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,18 @@ import useFPClient from './useFPClient';
 import useFPToggles from './useFPToggles';
 import { withFPConsumer } from './withFPConsumer';
 import AsyncFPProvider from './asyncFPProvider';
+import { getPlatform, initializePlatform } from 'featureprobe-client-sdk-js';
+import { version } from '../package.json';
 
 export { FeatureProbe, FPUser } from 'featureprobe-client-sdk-js';
 export type { FPDetail, FPConfig, FPStorageProvider } from 'featureprobe-client-sdk-js';
+
+initializePlatform({
+  platform: {
+    ...getPlatform(),
+    UA: 'REACT/' + version,
+  }
+});
 
 export {
   FPProvider,
